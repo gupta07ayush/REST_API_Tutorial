438c4843480a495d74bbb104ee0d17fb202f04b2
Ayush Gupta <gupta07ayush@gmail.com>
2023-05-15 19:18:22 +0530

    override str of company model to show company name in employee api table

--- api/models.py
+++ api/models.py
@@ -18,6 +18,9 @@ class Company(models.Model):
     added_date = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
 
+    def __str__(self):
+        return self.name
+
 # Employee Model
 
 
