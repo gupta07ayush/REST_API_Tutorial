a5aa8056dc4eb1616e4eab0812b5e3170f452f37
Ayush Gupta <gupta07ayush@gmail.com>
2023-05-15 19:03:06 +0530

    serializer for employee

--- api/serializers.py
+++ api/serializers.py
@@ -1,11 +1,23 @@
 from rest_framework import serializers
-from api.models import Company
+from api.models import Company, Employee
 
 
 # create serializers here
+
+# serializer for company
 class CompanySerializer(serializers.HyperlinkedModelSerializer):
     company_id = serializers.ReadOnlyField()
 
     class Meta:
         model = Company
+        fields = "__all__"
+
+# serializer for Employee
+
+
+class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Employee
         fields = "__all__"
