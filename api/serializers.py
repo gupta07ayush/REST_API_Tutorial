fbe0292ae888f2279f6d57ed57cd7bfd98935b09
Ayush Gupta <gupta07ayush@gmail.com>
2023-05-15 18:50:16 +0530

    expose company id in api

--- api/serializers.py
+++ api/serializers.py
@@ -4,6 +4,8 @@ from api.models import Company
 
 # create serializers here
 class CompanySerializer(serializers.HyperlinkedModelSerializer):
+    company_id = serializers.ReadOnlyField()
+
     class Meta:
         model = Company
         fields = "__all__"
